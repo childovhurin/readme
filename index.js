aec7a91f60db55e7ab8a66f265dacad24a4153c0
Megan Murphy <childovhurin@gmail.com>
2020-04-11 16:26:43 -0400

    added questArray, updated readme, node modules and json

---
+++ index.js
@@ -0,0 +1,34 @@
+//libraries
+const ask = require("inquirer");
+const fs = require("fs");
+const axios = require("axios");
+
+//questions array
+const questArray = [
+    "What is your Github username?", 
+    "What is the name of your project?",
+    "What is your email address?",
+    "Describe your project:",
+    "How do you install this project?",
+    "How is this project used?",
+    "What licenses are used for this project?",
+    "What tests were performed on this project?",
+    "Where would users direct thier questions?",
+]
+
+
+
+
+
+
+
+
+[{
+    type: 'input';
+    name: 'username';
+    message: questions
+
+
+
+
+}]
