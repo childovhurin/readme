1b67dd76a1b965d94b16da949dd75ae5c02bb007
Megan Murphy <childovhurin@gmail.com>
2020-04-11 20:22:14 -0400

    functions are operational, api.js updated

--- Develop/index.js
+++ Develop/index.js
@@ -1,77 +1,69 @@
-//libraries
-const ask = require("inquirer");
-const fs = require("fs");
+const inquirer = require("inquirer");
+const fs = require('fs');
 const axios = require("axios");
+const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-//questions array
-const questions = [{
+const questions = [
+  {
     type: "input",
-    name: "name",
-    message: "What is your Github username?"
-},
-{
+    message: "What is your Github username?",
+    name: "username"
+  },
+  {
     type: "input",
-    name: "project",
-    message: "What is the name of your project?"
-},
-{
+    message: "What is your email?",
+    name: "email"
+  },
+  {
     type: "input",
-    name: "email",
-    message: "What is your email address?"
-},
-{
+    message: "What is the project title?",
+    name: "title"
+  },
+  {
     type: "input",
-    name: "project",
-    message: "Describe your project:"
-},
-{
+    message: "Give a brief description of this project.",
+    name: "description"
+  },
+  {
     type: "input",
-    name: "install",
-    message: "How do you install this project?"
-},
-{
+    message: "How do you install this project?",
+    name: "installation"
+  },
+  {
     type: "input",
-    name: "use",
-    message: "How is this project used?"
-},
-{
-    type: "input",
-    name: "license",
-    message: "What licenses are used for this project?"
-},
-{
-    type: "input",
-    name: "tests",
-    message: "What tests were performed on this project?"
-},
-{
-    type: "input",
-    name: "tests",
-    message: "Where would users direct thier questions?"
-}].then(answers => {
-    console.log("Good job!")
-    console.log(answers);
-
-    var ans = JSON.stringify(answers);
-    console.log(ans);
-    console.log(answers.name);
-});
+    message: "What licenses does this project have?",
+    name: "license"
+  },
+ 
+];
 
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, err => {
+
+    if (err) {
+      return console.log(err);
+    }else {
+
+    console.log("Success!")}
+
+  })
 }
 
-fs.writeFileSync("readme.md", ans);
+function start() {
+  inquirer
+    .prompt(questions) 
+    .then(answers => { 
+      api.getUser(answers.username)
+        .then(data => {
+          console.log(api)
+ //Write file 
+          writeToFile("README.md", generateMarkdown(...answers, ...data));
+        });
+    })
+    .catch(error => {
+      console.log("error")
+    });
+};
 
-// .catch (error => {
-//     console.log("error!");
-// )
-// };
-
-
-
-
-function init() {
-
-}
-
-init();
+start();
